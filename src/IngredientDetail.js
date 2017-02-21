4508b02c2d2e280086db177cdb63646a00ff90a4
adventurebear <suzanneatkinson@gmail.com>
2017-02-20 20:42:31 -0500

    editing of ingredients layout done

--- src/IngredientDetail.js
+++ src/IngredientDetail.js
@@ -1,147 +1,112 @@
 import React, { Component } from 'react'
 import './css/IngredientDetail.css'
+import edit from './images/edit.png'
+import del from './images/delete.png'
+import save from './images/save.png'
+import back from './images/return.png'
 
 class IngredientDetail extends Component {
-    renderDisplay(){
-        return (
-            <div className='component-ingredientdetail'>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th className="right">Amt</th>
-                            <th>Unit</th>
-                            <th>Ingredient</th>
-                        </tr>
-                    {this.props.ingredientList.map((ingredient, i)=>{
-                        return (
-                          <tr key={i}>
-                            <td className="right">{ingredient.amount}</td>
-                            <td>{ingredient.unit}</td>
-                            <td>{ingredient.ingredient}</td>
-                          </tr>
-                        )}
-                    )}
-                    </tbody>
-                </table>
-            </div>
-        )
+  constructor (props) {
+    super(props)
+    this.state = {
+      isEditing: false,
+      ingredient: this.props.ingredient
+
     }
+  }
+  handleEditClick = () =>{
+    this.setState ({
+      isEditing: true
+    })
+  }
+  handleInputChange = (event) => {
+    console.log('ingredient edited', event.target.value)
+    this.setState({
+      ingredient: event.target.value
+    })
+  }
+  handleBackClick = () => {
+    this.setState ({
+      isEditing:false
+    })
+  }
+  handleSaveClick = () => {
+    console.log('Save Button Clicked, ' + this.state.ingredient)
+    let newIngredient = this.state.ingredient
+    this.setState({
+      isEditing: false
+    })
 
-    renderEdit(){
-        return (
-          <div className='component-ingredientdetail'>
-              <table>
-                  <tbody>
-                  <tr>
-                      <th className="right">Amt</th>
-                      <th>Unit</th>
-                      <th>Ingredient</th>
-                      <th></th>
-                  </tr>
+    //this.props.saveIngredient(newIngredient, this.props.index)
 
-                  {this.props.ingredientList.map((ingredient, i)=>{
-                      return (
-                    <tr key={i}>
-                      <td className="right">
-                          <input className="skinny" type="text"
-                                 defaultValue={ingredient.amount}
-                                 onChange={this.props.handleAmtChange} />
-                      </td>
-                        <td><select  defaultValue={ingredient.unit}
-                                     onChange={this.props.handleUnitChange} >
-                            <option value="teaspoon">tsp</option>
-                            <option value="tbsp">tbsp</option>
-                            <option value="lb">lb</option>
-                            <option value="cup">cup</option>
-                            <option value="oz">oz</option>
-                            <option value="quart">quart</option>
-                            <option value="gallon">gallon</option>
-                            <option value="lb">lb</option>
-                            <option value="ml">ml</option>
-                            <option value="gm">gm</option>
-                            <option value="liter">liter</option>
-                            <option value="whole">whole</option>
-                            <option value="stick">stick</option>
-                            <option value="large">large</option>
-                            <option value="med">med</option>
-                            <option value="small">small</option>
-                            <option value="clove">clove</option>
-                            <option value="pinch">pinch</option>
-                            <option value="dash">dash</option>
-                            <option value="small">small</option>
-                            </select>
-                        </td>
-                        <td><input type="text"
-                                   defaultValue={ingredient.ingredient}
-                                   onChange={this.props.handleIngredientChange} />
-                        </td>
-                        <td className="i i-delete">x</td>
-                        </tr>
-                      )
-                  })}
-
-                    <tr>
-                        <td className="right">
-                          <input  className="skinny"
-                                  type="text"
-                                   placeholder="Amt"
-                                   onChange={this.props.handleAmtChange} />
-                        </td>
-                        <td><select onChange={this.props.handleUnitChange}>
-                            <option value="teaspoon">tsp</option>
-                            <option value="tbsp">tbsp</option>
-                            <option value="lb">lb</option>
-                            <option value="cup">cup</option>
-                            <option value="oz">oz</option>
-                            <option value="quart">quart</option>
-                            <option value="gallon">gallon</option>
-                            <option value="lb">lb</option>
-                            <option value="ml">ml</option>
-                            <option value="gm">gm</option>
-                            <option value="liter">liter</option>
-                            <option value="whole">whole</option>
-                            <option value="stick">stick</option>
-                            <option value="large">large</option>
-                            <option value="med">med</option>
-                            <option value="small">small</option>
-                            <option value="clove">clove</option>
-                            <option value="pinch">pinch</option>
-                            <option value="dash">dash</option>
-                            <option value="small">small</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input type="text"
-                                   placeholder="ingredient"
-                                   onChange={this.props.handleIngredientChange} />
-                        </td>
-                        <td className="i i-add">+</td>
-
-                    </tr>
-                  </tbody>
-              </table>
-          </div>
-        )
+  }
+  render () {
+    if (this.state.isEditing) {
+      return this.renderEdit()
+    } else {
+      return this.renderDisplay()
     }
+  }
 
-    render(){
-        if (this.props.isEditing){
-            return this.renderEdit()
-        } else {
-            return this.renderDisplay()
-        }
-    }
+  renderDisplay () {
+    return (
+      <tr className='component-ingredientdetail'>
+        <td className="amt right">{this.props.ingredient.amount}</td>
+        <td className="unit">{this.props.ingredient.unit}</td>
+        <td className="ing">{this.props.ingredient.ingredient}</td>
+        <td className="action"><div className="tooltip"><span className="tooltiptext">Edit</span><img src={edit} onClick={this.handleEditClick} className="edit" alt="edit"/></div></td>
+        <td className="action"><div className="tooltip"><span className="tooltiptext">Delete</span><img src={del} className="del" alt="delete"/></div></td>
+      </tr>
+    )
+  }
+
+
+  renderEdit(){
+    return (
+              <tr className='component-ingredientdetail'>
+                <td className="amt right">
+                  <input className="amt" type="text"
+                         defaultValue={this.props.ingredient.amount}
+                         onChange={this.handleAmtChange} />
+                </td>
+                <td className="unit">
+                  <select  defaultValue={this.props.ingredient.unit}
+                           onChange={this.handleUnitChange} >
+                    <option value="teaspoon">tsp</option>
+                    <option value="tbsp">tbsp</option>
+                    <option value="lb">lb</option>
+                    <option value="cup">cup</option>
+                    <option value="oz">oz</option>
+                    <option value="quart">quart</option>
+                    <option value="gallon">gallon</option>
+                    <option value="lb">lb</option>
+                    <option value="ml">ml</option>
+                    <option value="gm">gm</option>
+                    <option value="liter">liter</option>
+                    <option value="whole">whole</option>
+                    <option value="stick">stick</option>
+                    <option value="large">large</option>
+                    <option value="med">med</option>
+                    <option value="small">small</option>
+                    <option value="clove">clove</option>
+                    <option value="pinch">pinch</option>
+                    <option value="dash">dash</option>
+                    <option value="small">small</option>
+                  </select>
+                </td>
+                <td className="ing"><input type="text"
+                           defaultValue={this.state.ingredient.ingredient}
+                           onChange={this.handleInputChange} />
+                </td>
+                <td className="action"><div className="tooltip"><span className="tooltiptext">Save</span><img src={save} onClick={this.handleSaveClick} className="save" alt="save"/></div></td>
+                <td className="action"><div className="tooltip"><span className="tooltiptext">Cancel</span><img src={back} onClick={this.handleBackClick} className="back" alt="back"/></div></td>
+              </tr>
+            )}
+
 }
-
 IngredientDetail.propTypes={
-    ingredientList: React.PropTypes.array,
-    isEditing: React.PropTypes.bool
+  ingredient: React.PropTypes.array,
+  saveIngredient: React.PropTypes.func
 }
 
 export default IngredientDetail
-
-/* to implement
- handleUnitChange: React.PropTypes.func,
- handleAmtChange:React.PropTypes.func,
- handleIngredientChange: React.PropTypes.func
- */
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import IngredientDetail from './IngredientDetail'
 import './css/IngredientList.css'
-import edit from './images/edit.png'
-import del from './images/delete.png'
+
 
 class IngredientList extends Component {
           constructor(props){
@@ -11,149 +10,36 @@ class IngredientList extends Component {
                isEditing: false
               }
           }
-          handleEditClick = () =>{
-            this.setState ({
-              isEditing: true
-            })
+          saveRecipe = (newIng, i) => {
+            console.log("Save Button Clicked in ingredients")
+
           }
 
-  
-          renderDisplay(){
-              return (
-
-              <div className='component-ingredientdetail'>
-
-                <h3>Ingredients</h3>
-                <table>
-                <tbody>
-                  <tr>
-                  <th className="right">Amt</th>
-                  <th>Unit</th>
-                  <th>Ingredient</th>
-                  </tr>
-            {this.props.ingredientList.map((ingredient, i)=>{
-                return (
-                  <tr key={i}>
-                    <td className="right">{ingredient.amount}</td>
-                    <td>{ingredient.unit}</td>
-                    <td>{ingredient.ingredient}</td>
-                    <td><img src={edit} onClick={this.handleEditClick} className="edit" alt="edit" /></td>
-                    <td><img src={del} className="del" alt="delete" /></td>
-                  </tr>
-                )}
-            )}
-                </tbody>
-              </table>
-              </div>
-              )
-            }
-
-              renderEdit(){
-              return (
-
-              <div className='component-ingredientdetail'>
-                <h3>Ingredients</h3>
+        render() {
+          return (
+            <div>
+              <h3>Ingredients</h3>
               <table>
-              <tbody>
-              <tr>
-              <th className="right">Amt</th>
-              <th>Unit</th>
-              <th>Ingredient</th>
-              <th></th>
-              </tr>
+                <tbody>
+                <tr>
+                  <th className="amt right">Amt</th>
+                  <th className="unit">Unit</th>
+                  <th className="ing">Ingredient</th>
+                  <th className="action"></th>
+                  <th className="action"></th>
+                </tr>
 
-              {this.props.ingredientList.map((ingredient, i)=>{
-                return (
-                  <tr key={i}>
-                    <td className="right">
-                      <input className="skinny" type="text"
-                             defaultValue={ingredient.amount}
-                             onChange={this.handleAmtChange} />
-                    </td>
-                    <td>
-                      <select  defaultValue={ingredient.unit}
-                                 onChange={this.handleUnitChange} >
-                        <option value="teaspoon">tsp</option>
-                        <option value="tbsp">tbsp</option>
-                        <option value="lb">lb</option>
-                        <option value="cup">cup</option>
-                        <option value="oz">oz</option>
-                        <option value="quart">quart</option>
-                        <option value="gallon">gallon</option>
-                        <option value="lb">lb</option>
-                        <option value="ml">ml</option>
-                        <option value="gm">gm</option>
-                        <option value="liter">liter</option>
-                        <option value="whole">whole</option>
-                        <option value="stick">stick</option>
-                        <option value="large">large</option>
-                        <option value="med">med</option>
-                        <option value="small">small</option>
-                        <option value="clove">clove</option>
-                        <option value="pinch">pinch</option>
-                        <option value="dash">dash</option>
-                        <option value="small">small</option>
-                    </select>
-                    </td>
-                    <td><input type="text"
-                               defaultValue={ingredient.ingredient}
-                               onChange={this.handleIngredientChange} />
-                    </td>
-                    <td className="i i-delete">x</td>
-                  </tr>
-                )
-              })}
+                {this.props.ingredientList.map((ingredient, i) => {
+                console.log(ingredient)
+                return <IngredientDetail key={i} ingredient={ingredient} />
 
-              <tr>
-              <td className="right">
-              <input  className="skinny"
-              type="text"
-              placeholder="Amt"
-              onChange={this.handleAmtChange} />
-              </td>
-                <td><select onChange={this.handleUnitChange}>
-                  <option value="teaspoon">tsp</option>
-                  <option value="tbsp">tbsp</option>
-                  <option value="lb">lb</option>
-                  <option value="cup">cup</option>
-                  <option value="oz">oz</option>
-                  <option value="quart">quart</option>
-                  <option value="gallon">gallon</option>
-                  <option value="lb">lb</option>
-                  <option value="ml">ml</option>
-                  <option value="gm">gm</option>
-                  <option value="liter">liter</option>
-                  <option value="whole">whole</option>
-                  <option value="stick">stick</option>
-                  <option value="large">large</option>
-                  <option value="med">med</option>
-                  <option value="small">small</option>
-                  <option value="clove">clove</option>
-                  <option value="pinch">pinch</option>
-                  <option value="dash">dash</option>
-                  <option value="small">small</option>
-                </select>
-                </td>
-              <td>
-              <input type="text"
-              placeholder="ingredient"
-              onChange={this.handleIngredientChange} />
-              </td>
-              <td className="i i-add">+</td>
+              })
+              }
 
-              </tr>
-              </tbody>
-              </table>
-              </div>
-              )
-            }
-
-        render(){
-          if (this.state.isEditing){
-          return this.renderEdit()
-        } else {
-          return this.renderDisplay()
-        }
+                </tbody>
+                </table>
+            </div>
+          )
         }
 
 
@@ -161,13 +47,13 @@ class IngredientList extends Component {
 
 }
 
-IngredientList.propTypes = {
-    ingredientList: React.PropTypes.array
+  IngredientList.propTypes = {
+    ingredientList: React.PropTypes.object
 }
 
 
 
-export default IngredientList
+  export default IngredientList
 
 /* to implement
  handleUnitChange: React.PropTypes.func,
